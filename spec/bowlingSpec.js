af887a5581ea77cb7c42692e5aa89a2af2e9685b
Josh Bebbington <josh@focalwebsites.com>
2015-01-23 12:00:54 +0000

    At the start of a game, the score is 0

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -2,4 +2,13 @@ describe("Scorecard", function() {
   
   var scorecard;
 
+  describe("At the start of a game", function() {
+    
+    it("Has a default score of 0", function() {
+      scorecard = new Scorecard();
+      expect(scorecard.totalScore).toEqual(0);
+    });
+
+  });
+
 });
