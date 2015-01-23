d00319e0662aff54716add72ed417d4797546d0d
Josh Bebbington <josh@focalwebsites.com>
2015-01-23 12:10:08 +0000

    A player can be added to a game

--- js/bowling.js
+++ js/bowling.js
@@ -1,3 +1,8 @@
 var Scorecard = function() {
   this.totalScore = 0;
+  this.playerName = 'nil';
+};
+
+Scorecard.prototype.addPlayer = function(name) {
+  this.playerName = name;
 };
--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -2,11 +2,23 @@ describe("Scorecard", function() {
   
   var scorecard;
 
+  beforeEach(function() {
+    scorecard = new Scorecard();
+  });
+
   describe("At the start of a game", function() {
     
     it("Has a default score of 0", function() {
-      scorecard = new Scorecard();
       expect(scorecard.totalScore).toEqual(0);
+    });
+
+    it("Doesn't have a player", function() {
+      expect(scorecard.playerName).toEqual('nil');
+    });
+
+    it("Can have a player added", function() {
+      scorecard.addPlayer('Josh');
+      expect(scorecard.playerName).toEqual('Josh');
     });
 
   });
